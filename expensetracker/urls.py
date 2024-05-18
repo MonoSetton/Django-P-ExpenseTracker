aafe2ce39f3009bf462fe389460c1af99640360a
MonoSetton <maksymiliankloch@gmail.com>
2024-05-18 13:39:04 +0200

    Added htmls for password recovery etc.

--- expensetracker/urls.py
+++ expensetracker/urls.py
@@ -3,9 +3,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts.decorators import unauthenticated_user
+from django.contrib.auth.views import LoginView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', unauthenticated_user(LoginView.as_view()), name='login'),
     path('', include('accounts.urls')),
     path('', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
